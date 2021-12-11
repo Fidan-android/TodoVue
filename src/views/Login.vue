2931831d8c931a4b50be3afebce5411f82b7f728
Fidan-android <gum.fidan2017@yandex.ru>
2021-12-11 22:44:30 +0300

    try2

---
+++ src/views/Login.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="wrapper login">
+        <img src="../assets/logo.jpg" alt="Logo" class="logo">
+        <form class="form" @submit="onSubmit">
+            <input type="email" placeholder="Email" name="email" class="field" required>
+            <input type="password" placeholder="Password" name="password" class="field" id="pass" required>
+            <div id="eye">
+                <a href="#" class="eye_element" id="show"></a>
+                <a href="#" class="eye_line" id="hide"></a>
+            </div>
+
+            <router-link to="/password-recovery" class="ret-but">Forgot Password?</router-link>
+            <input type="submit" value="sign in">
+        </form>
+        <label>Don't have an account?&nbsp;<router-link to="/registration">Sign up</router-link></label>
+    </div>
+</template>
+
+<script>
+    import { login } from '../helpers/fetch';
+    export default {
+        name: "Login",
+        methods: {
+            onSubmit: function(e) {
+                e.preventDefault();
+                console.log("hello");
+            }
+        }
+}
+</script>
