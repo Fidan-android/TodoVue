6af8d25e66764fc6fa943fff5b3b1863b966271b
Fidan-android <gum.fidan2017@yandex.ru>
2021-12-11 23:06:26 +0300

    FetchRequest

--- src/helpers/fetch.js
+++ src/helpers/fetch.js
@@ -1,3 +1,15 @@
 export function login(forma) {
-    var response = fetch("");
+    var response = await fetch("http://a91745zj.beget.tech/signin/signin.php", {
+        body: JSON.stringify(Object.fromEntries((new FormData(forma)).entries()))
+    });
+
+    return response.json();
+}
+
+export function registration(forma) {
+    var response = await fetch("http://a91745zj.beget.tech/signup/signup.php", {
+        body: JSON.stringify(Object.fromEntries((new FormData(forma)).entries()))
+    });
+
+    return response.json();
 }
